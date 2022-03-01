f0dee2efef83a7849f7e1b2143fc03f843e8cc7b
Colin Monaghan <cleemonaghan@gmail.com>
2022-03-01 14:45:40 -0800

    Removed unused imports

--- src/components/quizBox.jsx
+++ src/components/quizBox.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { Button } from "react-bootstrap";
 
 function QuizBox({ name, description }) {
-  return (
-    <div className="quiz-box">
-      <Button variant="dark p-0 col-12">
-        <div className="group-name bg-dark text-white px-3 py-3">
-          <h2>{name}</h2>
-          <p>{description}</p>
-        </div>
-      </Button>{" "}
-    </div>
-  );
+	return (
+		<div className="quiz-box">
+			<Button variant="dark p-0 col-12">
+				<div className="group-name bg-dark text-white px-3 py-3">
+					<h2>{name}</h2>
+					<p>{description}</p>
+				</div>
+			</Button>{" "}
+		</div>
+	);
 }
 
 export default QuizBox;

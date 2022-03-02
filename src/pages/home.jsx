2e1faf795add72edbb6cdeaee8d23500fca33b39
Chloe Thies <chloe.r.thies@gmail.com>
2022-03-01 19:52:19 -0800

    fixed spacing issue

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,98 +5,100 @@ import GroupBox from "../components/groupBox";
 import { photo13, photo14, photo15, photo16, photo17 } from "../images";
 
 class Home extends React.Component {
-	constructor() {
-		super();
-		this.state = {};
-	}
+  constructor() {
+    super();
+    this.state = {};
+  }
 
-	async componentDidMount() {
-		const response = await Auth.currentAuthenticatedUser();
-		let user = response.username;
-		const image = await Storage.get(user + "_profile_pic");
-		this.setState({ username: user, profile_pic: image });
-	}
+  async componentDidMount() {
+    const response = await Auth.currentAuthenticatedUser();
+    let user = response.username;
+    const image = await Storage.get(user + "_profile_pic");
+    this.setState({ username: user, profile_pic: image });
+  }
 
-	render() {
-		return (
-			<div className="home">
-				<div className="container">
-					<div className="float-end col-3">
-						<FriendsList />
-					</div>
-					<div className="row align-items-center my-5">
-						<div className="col-1">
-							<img
-								className="img-fluid rounded-circle mb-4 mb-0"
-								src={this.state.profile_pic}
-								alt=""
-							/>
-						</div>
-						<h3 className="font-weight-light col-3">{this.state.username}</h3>
-					</div>
-					<div className="row align-items-center mt-5 mb-2">
-						<h1 className="font-weight-bold col-4">Your Groups</h1>
-					</div>
-					<div className="row col-9 pb-5">
-						<div className="col-4 mb-4">
-							<GroupBox link={photo13} name="Hogwarts" />
-						</div>
-						<div className="col-4 mb-4">
-							<GroupBox link={photo14} name="Puppies" />
-						</div>
-						<div className="col-4 mb-4">
-							<GroupBox link={photo15} name="Astronomy" />
-						</div>
-					</div>
-					<div className="row col-9 pb-5">
-						<div className="col-4 mb-4">
-							<GroupBox link={photo16} name="Candy" />
-						</div>
-						<div className="col-4 mb-4">
-							<GroupBox link={photo17} name="Books" />
-						</div>
-					</div>
-					<div className="row align-items-center mt-5 mb-2">
-						<h1 className="font-weight-bold col-4">Your Quizzes</h1>
-					</div>
-					<div className="row col-9 pb-5">
-						<div className="col-4">
-							<QuizBox
-								name="Hogwarts"
-								description="description of the quiz goes here."
-							/>
-						</div>
-						<div className="col-4">
-							<QuizBox
-								name="Puppies"
-								description="description of the quiz goes here."
-							/>
-						</div>
-						<div className="col-4">
-							<QuizBox
-								name="Astronomy"
-								description="description of the quiz goes here."
-							/>
-						</div>
-					</div>
-					<div className="row col-9 pb-5">
-						<div className="col-4">
-							<QuizBox
-								name="Candy"
-								description="description of the quiz goes here."
-							/>
-						</div>
-						<div className="col-4">
-							<QuizBox
-								name="Books"
-								description="description of the quiz goes here."
-							/>
-						</div>
-					</div>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="home">
+        <div className="container">
+          <div className="float-end col-3">
+            <FriendsList />
+          </div>
+          <div className="row align-items-center my-5">
+            <div className="col-1">
+              <img
+                className="img-fluid rounded-circle my-auto"
+                src={this.state.profile_pic}
+                alt=""
+              />
+            </div>
+            <h3 className="font-weight-light col-3 my-auto">
+              {this.state.username}
+            </h3>
+          </div>
+          <div className="row align-items-center mt-5 mb-2">
+            <h1 className="font-weight-bold col-4">Your Groups</h1>
+          </div>
+          <div className="row col-9 pb-5">
+            <div className="col-4 mb-4">
+              <GroupBox link={photo13} name="Hogwarts" />
+            </div>
+            <div className="col-4 mb-4">
+              <GroupBox link={photo14} name="Puppies" />
+            </div>
+            <div className="col-4 mb-4">
+              <GroupBox link={photo15} name="Astronomy" />
+            </div>
+          </div>
+          <div className="row col-9 pb-5">
+            <div className="col-4 mb-4">
+              <GroupBox link={photo16} name="Candy" />
+            </div>
+            <div className="col-4 mb-4">
+              <GroupBox link={photo17} name="Books" />
+            </div>
+          </div>
+          <div className="row align-items-center mt-5 mb-2">
+            <h1 className="font-weight-bold col-4">Your Quizzes</h1>
+          </div>
+          <div className="row col-9 pb-5">
+            <div className="col-4">
+              <QuizBox
+                name="Hogwarts"
+                description="description of the quiz goes here."
+              />
+            </div>
+            <div className="col-4">
+              <QuizBox
+                name="Puppies"
+                description="description of the quiz goes here."
+              />
+            </div>
+            <div className="col-4">
+              <QuizBox
+                name="Astronomy"
+                description="description of the quiz goes here."
+              />
+            </div>
+          </div>
+          <div className="row col-9 pb-5">
+            <div className="col-4">
+              <QuizBox
+                name="Candy"
+                description="description of the quiz goes here."
+              />
+            </div>
+            <div className="col-4">
+              <QuizBox
+                name="Books"
+                description="description of the quiz goes here."
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Home;

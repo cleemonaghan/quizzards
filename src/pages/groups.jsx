533c24108a306f1db01156b5ba964be2149edb50
Colin Monaghan <cleemonaghan@gmail.com>
2022-03-19 13:04:18 -0700

    Updated groups page to display the groups the user is in

--- src/pages/groups.jsx
+++ src/pages/groups.jsx
@@ -5,7 +5,74 @@ import { MDBCol, MDBInput } from "mdbreact";
 import { Link } from "react-router-dom";
 import { photo13, photo14, photo15, photo16, photo17 } from "../images";
 
+import {
+  getUserGroups,
+} from "../databaseFunctions/users.js";
+
+import { Auth, Storage } from "aws-amplify";
+import { getGroup } from "../databaseFunctions/groups";
+
 class Groups extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      groupElements: null,
+    };
+  }
+
+  /** This method initalizes the state of the Group component.
+   *  
+   *  This method fetches the user's username and profile picture 
+   *  and initializes their values in the component's state.
+   */
+  async componentDidMount() {
+    //get the user information
+    const response = await Auth.currentAuthenticatedUser();
+    const username = response.username;
+    const yourGroups = await this.fetchYourGroups(username);
+
+    
+    // set the state with the user info
+    this.setState({
+      username: username, 
+    });
+
+    //update with the user's groups
+    this.setState({groupElements : yourGroups});   
+    
+    //update with the user's recommended groups
+  }
+
+  /** This method fetches and returns the list of groups that the user is in
+   * formatted in html form.
+   * 
+   * @param {String} username the user's username
+   * @returns a list of the html for each of their groups
+   */
+  async fetchYourGroups(username) {
+    const groupArr = await getUserGroups(username);
+    // if there are no groups, 
+    if (groupArr === undefined || groupArr.length < 1) {return (
+      <p>You have no groups</p>
+    );}
+    else {
+      //for each group we are in, fetch the group and add it to the result array
+      var result = [];
+      for(let i = 0; i < groupArr.length; i++) {
+        let group = await getGroup(groupArr[i].groupID);
+        let groupImage = await Storage.get(group.profilePicture);
+        result.push((<div className="col-lg-3 col-sm-6" key={i}>
+          <GroupBox key={group.id} link={groupImage} name={group.name} groupID={groupArr[i].groupID}/>
+        </div>)
+        );
+      }
+      return result;
+    }
+  }
+
+
   render() {
     return (
       <div className="groups">
@@ -31,7 +98,15 @@ class Groups extends React.Component {
               </Link>
             </div>
           </div>
-          {/* <hr /> */}
+          {/* Display the user's groups */}
+          <div className="row align-items-center mt-5 mb-2">
+            <h1 className="font-weight-bold">Your Groups</h1>
+          </div>
+          <div className="row">
+            {this.state.groupElements}
+          </div>
+          
+          {/* Display the user's recommended groups */}
           <div className="row align-items-center mt-5 mb-2">
             <h1 className="font-weight-bold">Suggested Groups</h1>
           </div>

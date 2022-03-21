3bfb3f1dce0701608c4b3d0d03a3b98a34b47d5b
Colin Monaghan <cleemonaghan@gmail.com>
2022-03-20 18:53:35 -0700

    Updated the loading screen to have a spinning wheel

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import { logo } from "../images/";
+import {Stack, Spinner} from "react-bootstrap"
 
 function Loading() {
   return (
-    <div>
-      <h1 className="w-responsive text-center mt-2 mb-0"> Loading... </h1>
-      {/*<img className="img-fluid" src={logo} alt="" />*/}
-    </div>
+    <Stack gap={2} className="col-md-4 mx-auto">
+      <div>
+        <div className="d-inline">
+        <Spinner animation="border" />
+        </div>
+        
+        <h1 className="d-inline w-responsive text-center ml-1 mt-2 mb-0"> Loading... </h1>
+      </div>
+      
+      <div>
+        <img className="img-fluid" src={logo} alt="" />
+      </div>
+      
+      
+    </Stack>
   );
 }
 

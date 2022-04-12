12d29fb9678e6c6ad4688bef8822fb979a935c6f
Chloe Thies <chloe.r.thies@gmail.com>
2022-04-11 20:53:40 -0700

    images

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,66 +1,66 @@
 import React from "react";
 import { Navbar, Container, Nav, Button } from "react-bootstrap";
 import {
-	HouseFill,
-	PeopleFill,
-	SendFill,
-	PersonCircle,
-	PatchQuestionFill,
+  HouseFill,
+  PeopleFill,
+  SendFill,
+  PersonCircle,
+  PatchQuestionFill,
 } from "react-bootstrap-icons";
-import logo from "../images/logo3.png";
+import logo from "../images/logo.png";
 import { Auth } from "aws-amplify";
 
 class Navigation extends React.Component {
-	async signout() {
-		try {
-			await Auth.signOut();
-		} catch (err) {
-			console.log("There was an error signing out: ", err);
-		}
-	}
-	render() {
-		return (
-			<Navbar bg="dark" variant="dark" sticky="top" className="mx0">
-				<Container>
-					<Navbar.Brand href="/">
-						<img
-							alt=""
-							src={logo}
-							width="27"
-							height="30"
-							className="d-inline-block"
-						/>{" "}
-						Quizzards
-					</Navbar.Brand>
-					<Nav>
-						<Nav.Link href="/">
-							<HouseFill className="mx-1 mb-1" />
-							Home
-						</Nav.Link>
-						<Nav.Link href="/quizzes">
-							<PatchQuestionFill className="mx-1 mb-1" />
-							Quizzes
-						</Nav.Link>
-						<Nav.Link href="/groups">
-							<PeopleFill className="mx-1 mb-1" />
-							Groups
-						</Nav.Link>
-						<Nav.Link href="/messages">
-							<SendFill className="mx-1 mb-1" />
-							Messages
-						</Nav.Link>
-						<Nav.Link href="/profile">
-							<PersonCircle className="mx-1 mb-1" />
-							Profile
-						</Nav.Link>
-						<Button variant="light" onClick={this.signout}>
-							Signout
-						</Button>
-					</Nav>
-				</Container>
-			</Navbar>
-		);
-	}
+  async signout() {
+    try {
+      await Auth.signOut();
+    } catch (err) {
+      console.log("There was an error signing out: ", err);
+    }
+  }
+  render() {
+    return (
+      <Navbar bg="dark" variant="dark" sticky="top" className="mx0">
+        <Container>
+          <Navbar.Brand href="/">
+            <img
+              alt=""
+              src={logo}
+              width="27"
+              height="30"
+              className="d-inline-block"
+            />{" "}
+            Quizzards
+          </Navbar.Brand>
+          <Nav>
+            <Nav.Link href="/">
+              <HouseFill className="mx-1 mb-1" />
+              Home
+            </Nav.Link>
+            <Nav.Link href="/quizzes">
+              <PatchQuestionFill className="mx-1 mb-1" />
+              Quizzes
+            </Nav.Link>
+            <Nav.Link href="/groups">
+              <PeopleFill className="mx-1 mb-1" />
+              Groups
+            </Nav.Link>
+            <Nav.Link href="/messages">
+              <SendFill className="mx-1 mb-1" />
+              Messages
+            </Nav.Link>
+            <Nav.Link href="/profile">
+              <PersonCircle className="mx-1 mb-1" />
+              Profile
+            </Nav.Link>
+            <Button variant="light" onClick={this.signout}>
+              Signout
+            </Button>
+          </Nav>
+        </Container>
+      </Navbar>
+    );
+  }
 }
 
 export default Navigation;

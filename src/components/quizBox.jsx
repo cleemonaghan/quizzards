cb31b6c45036c09b52f3c8f24c30e1c52b26968a
Chloe Thies <chloe.r.thies@gmail.com>
2022-04-13 16:53:11 -0700

    formatting

--- src/components/quizBox.jsx
+++ src/components/quizBox.jsx
@@ -1,12 +1,33 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { default_group } from "../images";
 
 function QuizBox({ name, description }) {
   return (
     <div className="quiz-box mb-4">
       <Link to="/quizPage" style={{ textDecoration: "none" }}>
         <Card className="text-center py-3" bg="dark" text="white">
+          {/* <Card.Img
+            variant="top"
+            // src={database.quiz_picture}
+            // alt={database.title}
+            src={default_group}
+            alt="..."
+          />
+          <Card.ImgOverlay
+            className="overlap_text align-items-center d-flex justify-content-center"
+            style={{
+              fontWeight: "bold",
+              // backgroundColor: "black",
+              // opacity: "0.2",
+            }}
+          >
+            <div>
+              <h2>{name}</h2>
+              <p>{description}</p>
+            </div>
+          </Card.ImgOverlay> */}
           <Card.Body>
             <Card.Title>
               <h2>{name}</h2>

da426c28e6713707053f4563d86c1eae99659b3d
Chloe Thies <chloe.r.thies@gmail.com>
2022-04-26 11:35:45 -0700

    Update statsBox.jsx

--- src/components/statsBox.jsx
+++ src/components/statsBox.jsx
@@ -2,18 +2,24 @@ import React, { useState, useEffect } from "react";
 import GroupStats from "./groupStats";
 import { Link } from "react-router-dom";
 import { Button } from "react-bootstrap";
-import { getQuiz, fetchUserAnswer } from "../databaseFunctions/quizzes";
+import {
+  getQuiz,
+  fetchUserAnswer,
+  getResult,
+} from "../databaseFunctions/quizzes";
 import Loading from "./loading";
 
-function useGatherResources(quizID, members) {
+function useGatherResources(quizID, members, username) {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
   const [memberResults, setMemberResults] = useState([]);
   const [quiz, setQuiz] = useState(null);
+  const [userResult, setUserResult] = useState(null);
 
   async function getInfo() {
     try {
       setLoading(true);
+
       //get the members' results
       let result = [];
       for (let i = 0; i < members.length; i++) {
@@ -28,6 +34,14 @@ function useGatherResources(quizID, members) {
       //get the quiz
       let quizRes = await getQuiz(quizID);
       setQuiz(quizRes);
+
+      // get the user's results
+      let res = await fetchUserAnswer(username, quizID);
+      if (res.length > 0) {
+        setUserResult(res[0]);
+      } else {
+        setUserResult(null);
+      }
     } catch (e) {
       //there was an error, so save it
       setError(e);
@@ -40,18 +54,27 @@ function useGatherResources(quizID, members) {
     getInfo();
   }, [quizID]);
 
-  return [memberResults, quiz, error, loading];
+  return [memberResults, quiz, error, loading, userResult];
 }
 
-function StatsBox({ group, quizID, result }) {
-  const [memberResults, quiz, error, loading] = useGatherResources(
+function getUserResult(userResult) {
+  if (userResult === null) {
+    return <div></div>;
+  } else {
+    return <p>{userResult.result}</p>;
+  }
+}
+
+function StatsBox({ group, quizID, result, username }) {
+  const [memberResults, quiz, error, loading, userResult] = useGatherResources(
     quizID,
-    group.members.items
+    group.members.items,
+    username
   );
 
   let data = [];
   let labels = [];
-  
+
   if (loading) {
     return Loading();
   } else {
@@ -77,7 +100,10 @@ function StatsBox({ group, quizID, result }) {
           </div>
           <hr />
           <div className="row mx-auto">
-            <h4 className="col-auto ms-2 my-2"> My Results: {result}</h4>
+            <h4 className="col-auto ms-2 my-2">
+              {" "}
+              My Results: {getUserResult(userResult)}
+            </h4>
             <div className="col-auto my-auto">
               <Link to={"/quizPage/" + quizID}>
                 <Button variant="outline-primary">Go to Quiz</Button>{" "}
@@ -89,5 +115,107 @@ function StatsBox({ group, quizID, result }) {
     );
   }
 }
+
+// function useGatherResources(quizID, members, username) {
+//   const [error, setError] = useState(null);
+//   const [loading, setLoading] = useState(true);
+//   const [memberResults, setMemberResults] = useState([]);
+//   const [quiz, setQuiz] = useState(null);
+//   const [userResult, setUserResult] = useState(null);
+
+//   async function getInfo() {
+//     try {
+//       setLoading(true);
+//       // get the user's results
+//       let res = await fetchUserAnswer(username, quizID);
+//       if (res.length > 0) {
+//         setUserResult(res[0]);
+//       } else {
+//         setUserResult(null);
+//       }
+
+//       //get the members' results
+//       let result = [];
+//       for (let i = 0; i < members.length; i++) {
+//         let res = await fetchUserAnswer(members[i].userID, quizID);
+//         if (res.length > 0) {
+//           //if the user has completed the quiz, add their result to the list
+//           result.push(res[0]);
+//         }
+//       }
+//       setMemberResults(result);
+
+//       //get the quiz
+//       let quizRes = await getQuiz(quizID);
+//       setQuiz(quizRes);
+//     } catch (e) {
+//       //there was an error, so save it
+//       setError(e);
+//     } finally {
+//       //we are finished loading, so set loading to false
+//       setLoading(false);
+//     }
+//   }
+//   useEffect(() => {
+//     getInfo();
+//   }, [quizID]);
+
+//   return [memberResults, quiz, error, loading, userResult];
+// }
+
+// // function getUserResult(userResult) {
+// //   if (userResult === null) {
+// //     return <div></div>;
+// //   } else {
+// //     return <p>{userResult}</p>;
+// //   }
+// // }
+
+// function StatsBox({ group, quizID, result }) {
+//   const [memberResults, quiz, error, loading] = useGatherResources(
+//     quizID,
+//     group.members.items
+//   );
+
+//   let data = [];
+//   let labels = [];
+
+//   if (loading) {
+//     return Loading();
+//   } else {
+//     for (let i in quiz.results.items) {
+//       labels.push(quiz.results.items[i].name);
+//       data.push(0);
+//     }
+//     for (let i = 0; i < memberResults.length; i++) {
+//       let result = memberResults[i].result;
+//       for (let j in quiz.results.items) {
+//         if (result === quiz.results.items[j].id) {
+//           data[j] += 1;
+//         }
+//       }
+//     }
+
+//     return (
+//       <div className="stats-box">
+//         <div className="outline mx-3 mb-5 pb-4">
+//           <h4 className="mx-3 my-3"> Group Stats: {quiz.title}</h4>
+//           <div className="mx-4 my-2">
+//             <GroupStats barLabels={labels} barData={data} />
+//           </div>
+//           <hr />
+//           <div className="row mx-auto">
+//             <h4 className="col-auto ms-2 my-2"> My Results: {result}</h4>
+//             <div className="col-auto my-auto">
+//               <Link to={"/quizPage/" + quizID}>
+//                 <Button variant="outline-primary">Go to Quiz</Button>{" "}
+//               </Link>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
 export default StatsBox;
